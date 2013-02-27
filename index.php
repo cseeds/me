650542e4a42b4089eae3d0bde9a0274129c2ac79
Chris Seeds <cseeds@gmail.com>
2013-02-27 00:26:10 -0500

    edits

--- index.php
+++ index.php
@@ -47,7 +47,6 @@ thanks for looking under the hood.
 	
 	
 	
-	
 	<!-- GOOGLE ANALYICS BEGIN -->
 	<script type="text/javascript">
 
@@ -60,8 +59,6 @@ thanks for looking under the hood.
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-</script>
-	<!-- GOOGLE ANALYICS END -->
 		
 	
 	
